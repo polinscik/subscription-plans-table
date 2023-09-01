b71371a2445f264b4caf7ff3cc894cb8998c986f
Polina <polinscik@gmail.com>
2023-09-01 18:07:31 +0300

    howto make it select one only

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.scss";
 import Fee from "./components/Fee";
 import JsonInfo from "./feeInfo.json";
+import {useState} from "react";
 
 function App() {
+  // const [oneSelected, checkOneSelected] = useState();
+
+  const handleChange = (event) => {
+    const fees = document.getElementsByClassName("fee_accented");
+    console.log(fees);
+    if (fees && !event.target.classList.contains("fee_accented")) {
+      fees[0].classList.remove("fee_accented");
+    }
+  };
   return (
     <div className="App">
       {JsonInfo.map((subPlan) => (
         <Fee
+          onClick={handleChange}
           key={subPlan.name}
           name={subPlan.name}
           price={subPlan.price}
