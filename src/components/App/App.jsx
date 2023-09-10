a3278d1374bb5e671e6f18a8de9ab077eb38e0c0
Polina <polinscik@gmail.com>
2023-09-10 14:56:07 +0300

    organize files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
-import Fee from "./components/Fee";
-import JsonInfo from "./feeInfo.json";
+import Fee from "../Fee/Fee";
+import JsonInfo from "../../feeInfo.json";
 import {useState} from "react";
 
 function App() {
